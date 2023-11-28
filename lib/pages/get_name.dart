0c45d5a692a65bd81d4f0b23f39551e37c0df7e4
Harpreet Singh Saluja <hssaluja2508@gmail.com>
2023-11-28 19:05:30 +0530

    adds ability to upload avatar while onboarding new users

--- lib/pages/get_name.dart
+++ lib/pages/get_name.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:learning_once_again/components/my_textfield.dart';
 import 'package:learning_once_again/pages/home_page.dart';
+import 'package:learning_once_again/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
 class GetName extends StatefulWidget {
   const GetName(
@@ -27,6 +33,7 @@ class _GetNameState extends State<GetName> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   late final Future fetchUserDoc;
   late final DocumentReference userRef;
+  String downloadUrl = '';
 
   @override
   void initState() {
@@ -58,6 +65,9 @@ class _GetNameState extends State<GetName> {
                             'email': widget.email,
                             'name': nameController.text,
                             'status': 'Hey there! 👋',
+                            'avatar': downloadUrl.isEmpty
+                                ? 'https://firebasestorage.googleapis.com/v0/b/learing-auth-mkay.appspot.com/o/images%2Fprofile_pictures%2Fdefault.png?alt=media&token=9771cf0b-9fa3-4b87-bb2b-21013636a2a5'
+                                : downloadUrl,
                           });
                           setState(() {
                             gottenName = true;
@@ -101,6 +111,72 @@ class _GetNameState extends State<GetName> {
                               hintText: widget.name,
                               obscureText: false,
                             ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 50),
+                      Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(left: 15),
+                            child: const Text(
+                              'Avatar',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          const SizedBox(width: 100),
+                          GestureDetector(
+                            onTap: () async {
+                              final result = await FilePicker.platform
+                                  .pickFiles(type: FileType.image);
+                              if (result == null) {
+                                // No file selected; do nothing
+                              } else {
+                                print('user has picked file');
+                                final storage = FirebaseStorage.instance.ref();
+                                final images = storage.child("images");
+                                final profiles =
+                                    images.child("profile_pictures");
+
+                                // Show the processing snackbar for a long time and after the process is complete, remove the snackbar
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    backgroundColor:
+                                        Color.fromARGB(255, 226, 1, 87),
+                                    content: Text('Processing'),
+                                    duration: Duration(minutes: 5),
+                                  ),
+                                );
+                                final file = result.files.first;
+                                final userProfile = profiles.child(widget.uid);
+                                final localFilePath = file.path;
+                                final uploadFile = File(localFilePath ?? '');
+                                // Upload to firebase storage and get the URL for the file
+                                await userProfile.putFile(uploadFile);
+                                final String myDownloadUrl =
+                                    await userProfile.getDownloadURL();
+                                print(myDownloadUrl);
+                                setState(() {
+                                  downloadUrl = myDownloadUrl;
+                                });
+                                // change the avatar
+                                Provider.of<UserProvider>(context,
+                                        listen: false)
+                                    .avatar = downloadUrl;
+                                ScaffoldMessenger.of(context)
+                                    .removeCurrentSnackBar();
+                              }
+                            },
+                            child: downloadUrl.isEmpty
+                                ? const CircleAvatar(
+                                    backgroundImage: AssetImage(
+                                        'assets/images/blank_avatar.png'),
+                                    radius: 70,
+                                  )
+                                : CircleAvatar(
+                                    backgroundImage: NetworkImage(downloadUrl),
+                                    radius: 70,
+                                  ),
                           ),
                         ],
                       ),
